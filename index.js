70de516f8aeedf092269a78cb5d7900f1c0ae336
Samson Mubeya <rasspartah@gmail.com>
2024-03-31 17:46:51 +0300

    did my assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here!
+let main=document.getElementById("main");
+
+console.log(main);
+console.log(document);
+//document.remove(main);
+//console.log(document);
+console.log(document.body.children);
+document.body.removeChild(main);
+console.log(document.body.children);
+let newHeader = document.createElement("h1");
+newHeader.textContent =("YOUR-NAME is the champion").toString();
+newHeader.id = "victory";
+document.body.append(newHeader);
+console.log(document.body);
+
+
+
+
+
+
+//console.log(document.body.h1)
+
+//console.log(document.body.h1)
+
+
+//console.log(document.body.h1.innerHTML);
+//document.body.h1.innerHTML="newHeader"
+//console.log(document.body.h1.innerHTML);
